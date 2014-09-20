8fffa6907329790eb89e4ea72b1986ee2e11a3ce
zemogle <edward@gomez.me.uk>
2014-09-20 10:01:39 +0100

    modularising camera snap

--- webtest.py
+++ webtest.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import Flask, render_template, request
 from time import sleep
 import glob
-import os
+import os, shutil
 from local_settings import DOWNLOAD_DIR, CURRENT_PATH, SNAPSHOT_DIR, MAX_FILES, LIVE
 if LIVE:
     import picamera
@@ -14,6 +14,16 @@ app = Flask(__name__)
 
 def getImageList():
     return map(os.path.basename, glob.glob(SNAPSHOT_DIR + '*.jpg'))
+
+def snap(filename):
+    camLock = True
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        # Camera warm-up time
+        time.sleep(2)
+        camera.capture(SNAPSHOT_DIR +filename)
+    camLock = False
+    return True
 
 @app.route("/",methods=['GET', 'POST'])
 def home():
@@ -27,14 +37,11 @@ def home():
         if camLock:
             return render_template('home.html', **templateData)
         filename = now.strftime("image-%Y-%m-%dT%H:%m:%s.jpg")
+        newfile = "latest.jpg"
+        snap(filename)
+        shutil.copy(SNAPSHOT_DIR+filename,SNAPSHOT_DIR+newfile)
         templateData['filename'] = filename
-        camLock = True
-        with picamera.PiCamera() as camera:
-            camera.resolution = (1024, 768)
-            # Camera warm-up time
-            time.sleep(2)
-            camera.capture(SNAPSHOT_DIR +filename)
-        camLock = False
+
     else:
         if len(images)>0:
             templateData['filename'] = images[0]
@@ -42,11 +49,7 @@ def home():
             templateData['filename']=None
     return render_template('home.html', **templateData)
 
-class snapper:
-    def GET(self):
-        filename = snap()
-        raise web.seeother('/')
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=5000, debug=True)
+   app.run(host='0.0.0.0', port=8080, debug=True)
